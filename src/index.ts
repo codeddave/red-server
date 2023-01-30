958fb0988acdf61c828cb2d3fc49230b4ecd5649
codeddave <drizzzydave@gmail.com>
2023-01-30 16:18:25 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,21 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "red",
-    type: "postgresql",
-    user: "postgres",
-    port: 5433,
-    password: "password123",
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroOrmConfig);
 
   //create post
   const post = orm.em.create(Post, {
     title: "First post",
   });
-
-  //insert post into database.
+  console.log(post);
+  /*   //insert post into database.
   await orm.em.persistAndFlush(post);
+
+  //
+  await orm.em.insert(Post, { title: "First post2",}) */
 };
 
 main();
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,7 +1,9 @@
+import { MikroORMOptions } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 
-export default {
+const options: Partial<MikroORMOptions> = {
+  migrations: {},
   entities: [Post],
   dbName: "red",
   type: "postgresql",
@@ -10,3 +12,8 @@ export default {
   password: "password123",
   debug: !__prod__,
 };
+
+/* {
+
+} as Parameters<typeof MikroOrm.init>[0]  gets the type that init expecvts for uts first parameter which is the options object */
+export default options;
