bcad561d271e6763abe3d80ece92a90ef9687b6a
codeddave <drizzzydave@gmail.com>
2023-02-01 17:00:28 +0100

    updated mikroorm config, set up migrations

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,19 +1,23 @@
 import { MikroORMOptions } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-
+import path from "path";
 const options: Partial<MikroORMOptions> = {
-  migrations: {},
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    glob: "!(*.d).{js,ts}",
+  },
   entities: [Post],
   dbName: "red",
   type: "postgresql",
   user: "postgres",
-  port: 5433,
-  password: "password123",
+  host: "localhost",
+  port: 5434,
+  password: "pass123",
   debug: !__prod__,
 };
 
-/* {
+/* { 
 
 } as Parameters<typeof MikroOrm.init>[0]  gets the type that init expecvts for uts first parameter which is the options object */
 export default options;
