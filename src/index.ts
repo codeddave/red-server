764ed24e233fb6cf0228fb2d8f380fe343e72659
codeddave <drizzzydave@gmail.com>
2023-02-03 12:29:50 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,22 @@ import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
+  //run migration
+  orm.getMigrator().up();
 
   //create post
   const post = orm.em.create(Post, {
     title: "First post",
   });
   console.log(post);
-  /*   //insert post into database.
+  //insert post into database.
   await orm.em.persistAndFlush(post);
+  /*   ;
 
   //
   await orm.em.insert(Post, { title: "First post2",}) */
 };
 
-main();
+main().catch((error) => console.log(error));
 
 console.log("Hello world");
